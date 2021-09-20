dea06eb26acd415460e10604821de0ba3918de0c
Nahla531 <nhlaalekkesh@gmail.com>
2021-09-20 06:01:23 +0300

    section styling

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Container, Row } from "react-bootstrap";
 import Listing from "./Listing";
+import * as Icon from "react-bootstrap-icons";
 
 const Section = ({ link, heading }) => {
   return (
-    <section className="container-width  grid-container">
+    <section className="container-width grid-container">
       <div className="homepage-main">
         <header className="section-header">
           <a href={link}>
@@ -12,7 +13,7 @@ const Section = ({ link, heading }) => {
           </a>
           <a href={link} className="view-more">
             <span className="view-more_text">View More</span>
-            <i className="fas fa-angle-right"></i>
+            <Icon.ChevronRight className="chev-right" size={13} />
           </a>
         </header>
         <Container>
@@ -20,7 +21,6 @@ const Section = ({ link, heading }) => {
             <Listing isLarge="false" />
           </Row>
         </Container>
-        {/* listing goes here grid view */}
       </div>
     </section>
   );
