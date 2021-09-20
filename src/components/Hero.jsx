9a0d53cdebc2f08e669247e19c93897b9b12050c
Nahla531 <nhlaalekkesh@gmail.com>
2021-09-20 03:26:44 +0300

    done with strucure and content part

---
+++ src/components/Hero.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="homepage-hero-banner container">
+      <div className="homepage-inner-container ">
+        <div className="header-image">
+          <a href="#">
+            <img
+              src="https://mediacdn.grabone.co.nz/asset/yDjZTGuDg1"
+              className="desktop-header-image"
+              alt="Spring Collection"
+            />
+          </a>
+        </div>
+        <div className="notification-container">
+          <strong>Covid 19 Update:</strong> We encourage you to shop as normal
+          for all your essential products. Non-essentials also available, but
+          shipping delays may occur. No pickup options available. Stay safe NZ!
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
