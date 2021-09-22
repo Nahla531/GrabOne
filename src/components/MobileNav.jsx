aa61b660a5bf5e90867014d612c2ea36885d4c38
Nahla531 <nhlaalekkesh@gmail.com>
2021-09-22 15:40:01 +0300

    fixed the first section grid with meda quries overriding

---
+++ src/components/MobileNav.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const MobileNav = () => {
+  return (
+    <Container fluid>
+      <Row>
+        <Col>1 of 1</Col>
+        <Col>1 of 1</Col>
+        <Col>1 of 1</Col>
+        <Col>1 of 1</Col>
+        <Col>1 of 1</Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default MobileNav;
