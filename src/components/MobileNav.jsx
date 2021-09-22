5e02a7c6fbb0ad73ef92a94386fee66bc3f12d59
Nahla531 <nhlaalekkesh@gmail.com>
2021-09-22 19:05:45 +0300

    styling and structure done

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -1,14 +1,50 @@
 import React from "react";
-
+import * as Icon from "react-bootstrap-icons";
+import { Container, Col, Row } from "react-bootstrap";
 const MobileNav = () => {
   return (
-    <Container fluid>
+    <Container id="mobile-nav" fluid>
       <Row>
-        <Col>1 of 1</Col>
-        <Col>1 of 1</Col>
-        <Col>1 of 1</Col>
-        <Col>1 of 1</Col>
-        <Col>1 of 1</Col>
+        <Col>
+          <a href="#" className="mobile-nav-link">
+            <div style={{ display: "inline-block" }}>
+              <Icon.House></Icon.House>
+            </div>
+            <span>Home</span>
+          </a>
+        </Col>
+        <Col>
+          <a href="#" className="mobile-nav-link">
+            <div style={{ display: "inline-block" }}>
+              <Icon.Search></Icon.Search>
+            </div>
+            <span>Search</span>
+          </a>
+        </Col>
+        <Col>
+          <a href="#" className="mobile-nav-link">
+            <div style={{ display: "inline-block" }}>
+              <Icon.WindowSidebar></Icon.WindowSidebar>
+            </div>
+            <span>Categories</span>
+          </a>
+        </Col>
+        <Col>
+          <a href="#" className="mobile-nav-link">
+            <div style={{ display: "inline-block" }}>
+              <Icon.Cart></Icon.Cart>
+            </div>
+            <span>Cart</span>
+          </a>
+        </Col>
+        <Col>
+          <a href="#" className="mobile-nav-link">
+            <div style={{ display: "inline-block" }}>
+              <Icon.Person></Icon.Person>
+            </div>
+            <span>Account</span>
+          </a>
+        </Col>
       </Row>
     </Container>
   );
